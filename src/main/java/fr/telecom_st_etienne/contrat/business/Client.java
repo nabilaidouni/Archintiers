834214d16d849547c51b59e1ac79b44b50e18369
nabilaidouni <nabil_laidouni@hotmail.fr>
2020-01-10 13:52:53 +0100

    unicité des identifiants

--- src/main/java/fr/telecom_st_etienne/contrat/business/Client.java
+++ src/main/java/fr/telecom_st_etienne/contrat/business/Client.java
@@ -2,6 +2,7 @@ package fr.telecom_st_etienne.contrat.business;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Client {
 	
 	private String nom;
 	
+	@Column(unique=true)
 	private String identifiant;
 	
 	private String mdp;
