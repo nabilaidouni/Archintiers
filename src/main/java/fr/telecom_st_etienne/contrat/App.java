fed3016bd7d808973115274212b2c9379983fcb1
antoine-peyrard <antoine.peyrard@telecom-st-etienne.fr>
2020-01-23 17:31:29 +0100

    deploy

--- src/main/java/fr/telecom_st_etienne/contrat/App.java
+++ src/main/java/fr/telecom_st_etienne/contrat/App.java
@@ -2,6 +2,8 @@ package fr.telecom_st_etienne.contrat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "fr.telecom_st_etienne.contrat")
 @EnableJpaRepositories(basePackages = "fr.telecom_st_etienne.contrat.dao", entityManagerFactoryRef = "entityManagerFactory")
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class App implements WebMvcConfigurer{
 	
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
